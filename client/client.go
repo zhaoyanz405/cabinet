24e1ccc48eb476184f45487124301aa246a7e8ca
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-05-19 15:49:39 +0800

    feat: add cabinet client

---
+++ client/client.go
@@ -0,0 +1,26 @@
+package client
+
+type Cmd struct {
+	Name  string
+	Key   string
+	Value string
+	Error error
+}
+
+type Client interface {
+	Run(cmd *Cmd)
+	PipelinedRun([]*Cmd)
+}
+
+func New(typ, server string) Client {
+	if typ == "redis" {
+		return newRedisClient(server)
+	}
+	if typ == "http" {
+		return newHTTPClient(server)
+	}
+	if typ == "tcp" {
+		return newTCPClient(server)
+	}
+	panic("unknown client type " + typ)
+}
