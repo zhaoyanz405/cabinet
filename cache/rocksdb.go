7c590aa3d64252714d520506e4c0bc21965ada5b
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-05-19 16:25:26 +0800

    feat: support to batch write

--- cache/rocksdb.go
+++ cache/rocksdb.go
@@ -10,14 +10,21 @@ import (
 	"regexp"
 	"runtime"
 	"strconv"
+	"time"
 	"unsafe"
 )
+
+type pair struct {
+	k string
+	v []byte
+}
 
 type rocksdbCache struct {
 	db *C.rocksdb_t
 	ro *C.rocksdb_readoptions_t
 	wo *C.rocksdb_writeoptions_t
 	e  *C.char
+	ch chan *pair
 }
 
 func newRocksdbCache() *rocksdbCache {
@@ -32,7 +39,10 @@ func newRocksdbCache() *rocksdbCache {
 	}
 
 	C.rocksdb_options_destroy(options)
-	return &rocksdbCache{db, C.rocksdb_readoptions_create(), C.rocksdb_writeoptions_create(), e}
+	c := make(chan *pair, 5000)
+	wo := C.rocksdb_writeoptions_create()
+	go write_func(db, c, wo)
+	return &rocksdbCache{db, C.rocksdb_readoptions_create(), wo, e, c}
 }
 
 func (c *rocksdbCache) Get(key string) ([]byte, error) {
@@ -50,16 +60,7 @@ func (c *rocksdbCache) Get(key string) ([]byte, error) {
 }
 
 func (c *rocksdbCache) Set(key string, value []byte) error {
-	k := C.CString(key)
-	defer C.free(unsafe.Pointer(k))
-
-	v := C.CBytes(value)
-	defer C.free(v)
-
-	C.rocksdb_put(c.db, c.wo, k, C.size_t(len(key)), (*C.char)(v), C.size_t(len(value)), &c.e)
-	if c.e != nil {
-		return errors.New(C.GoString(c.e))
-	}
+	c.ch <- &pair{key, value}
 	return nil
 }
 
@@ -94,4 +95,43 @@ func (c *rocksdbCache) GetStat() Stat {
 		}
 	}
 	return s
+}
+
+const BATCH_SIZE = 100
+
+func flush_batch(db *C.rocksdb_t, b *C.rocksdb_writebatch_t, o *C.rocksdb_writeoptions_t) {
+	var e *C.char
+	C.rocksdb_write(db, o, b, &e)
+	C.rocksdb_writebatch_clear(b)
+}
+
+func write_func(db *C.rocksdb_t, c chan *pair, o *C.rocksdb_writeoptions_t) {
+	count := 0
+	t := time.NewTimer(time.Second)
+	b := C.rocksdb_writebatch_create()
+	for {
+		select {
+		case p := <-c:
+			count++
+			key := C.CString(p.k)
+			value := C.CBytes(p.v)
+			C.rocksdb_writebatch_put(b, key, C.size_t(len(p.k)), (*C.char)(value), C.size_t(len(p.v)))
+			C.free(unsafe.Pointer(key))
+			C.free(value)
+			if count == BATCH_SIZE {
+				flush_batch(db, b, o)
+				count = 0
+			}
+			if !t.Stop() {
+				<-t.C
+			}
+			t.Reset(time.Second)
+		case <-t.C:
+			if count != 0 {
+				flush_batch(db, b, o)
+				count = 0
+			}
+			t.Reset(time.Second)
+		}
+	}
 }
